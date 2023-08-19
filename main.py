ce08befb2ed59b89d15421b3b75bc05ad969a72b
SpencerSedano <spencer.sv20@gmail.com>
2023-08-19 10:59:40 +0800

    commited

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ user_input = input("Enter a Chinese characters word: ")
 print(user_input)
 
 user_input_pinyin = pinyin(user_input)
-print(type(user_input_pinyin))
-user_input_pinyin = str(user_input_pinyin)
+
+user_input_pinyin_str = str(user_input_pinyin)
+
 print(user_input_pinyin)
-user_input_pinyin_formatted = "".join(c for c in user_input_pinyin if c.isalpha())
+print(' '.join(' '.join(l) for l in user_input_pinyin))
+user_input_pinyin_formatted = " ".join(c for c in user_input_pinyin_str if c.isalpha())
 print(user_input_pinyin_formatted)
